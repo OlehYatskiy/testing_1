55a08d9eea2498274fc2c79014a96029c6cd6935
Oleh <oleh@nor4-debian.nor4.com>
2020-09-07 00:01:54 +0300

    done with 'calc()'

--- calculator/index.js
+++ calculator/index.js
@@ -1,43 +1,39 @@
+
 const selectLength = document.getElementById('select-length');
 const selectWidth = document.getElementById('select-width');
 const button = document.getElementById('button');
 
 button.addEventListener('click', function(e) {
-    console.log(calc(15));
+    const result = calc(selectLength.value * selectWidth.value / 4);
+    value =
+    popup.classList.add('active');
 });
 
 
 arr = [8, 6];
 
-
+function compare(newResult, result) {
+    return newResult.rem <= result.rem ? newResult : result;
+}
 
 function calc(base, i = 0, arrInt = []) {
        const rem = base % arr[i];
        const int = (base - rem) / arr[i];
-        if (rem === 0) return {
-            rem: rem,
-            arr: arrInt.concat(int)
-        }
-       let newResult = {};
+       let result = {
+           rem: rem,
+           arr: arrInt.concat(int)
+       }
+       if (rem === 0) return result;
 
-       if (i === arr.length - 1) {
-           newResult = {
+       let newResult = compare({
                rem: Math.abs(rem - arr[i]),
                arr: arrInt.concat(int + 1)
-           }
-       } else {
-           newResult = calc(rem,i + 1, arrInt.concat(int));
+       }, result);
+
+       if (i !== arr.length - 1) {
+           newResult = compare(newResult, calc(rem,i + 1, arrInt.concat(int)));
+           newResult = compare(newResult, calc(base, i + 1, arrInt.concat(0)));
        }
-        if (newResult.rem <= rem) {
-            let result = newResult;
-            newResult = calc(base, i + 1);
 
-            return newResult;
-
-        } else {
-            return {
-                rem: rem,
-                arr: arrInt//arrInt.concat(int)
-            }
-        }
+       return newResult;
 }
